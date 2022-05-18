4c66879569c433919864bfa6668b5c9d78c5161b
Lenovo <asadbek.polatov.02@mail.ru>
2022-05-18 17:07:18 +0500

    Admin template editing successfully

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -25,18 +25,6 @@
 
         <ul class="navbar-nav flex-row align-items-center ms-auto">
             <!-- Place this tag where you want the button to render. -->
-            <li class="nav-item lh-1 me-3">
-                <a
-                    class="github-button"
-                    href="https://github.com/themeselection/sneat-html-admin-template-free"
-                    data-icon="octicon-star"
-                    data-size="large"
-                    data-show-count="true"
-                    aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
-                >Star</a
-                >
-            </li>
-
             <!-- User -->
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
@@ -99,64 +87,3 @@
         </ul>
     </div>
 </nav>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{{--<nav class="navbar navbar-header navbar-expand-lg" data-background-color="blue2">--}}
-
-{{--    <div class="container-fluid">--}}
-
-{{--        <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">--}}
-
-
-{{--            <li class="nav-item dropdown hidden-caret">--}}
-{{--                <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">--}}
-{{--                    <div class="avatar-sm">--}}
-{{--                        <img src="https://img.favpng.com/7/5/8/computer-icons-font-awesome-user-font-png-favpng-YMnbqNubA7zBmfa13MK8WdWs8.jpg" alt="..." class="avatar-img rounded-circle">--}}
-{{--                    </div>--}}
-{{--                </a>--}}
-{{--                <ul class="dropdown-menu dropdown-user animated fadeIn">--}}
-{{--                    <div class="dropdown-user-scroll scrollbar-outer">--}}
-{{--                        <li>--}}
-{{--                            <div class="user-box">--}}
-{{--                                <div class="avatar-lg"><img src="https://img.favpng.com/7/5/8/computer-icons-font-awesome-user-font-png-favpng-YMnbqNubA7zBmfa13MK8WdWs8.jpg" alt="image profile" class="avatar-img rounded"></div>--}}
-{{--                                <div class="u-text">--}}
-{{--                                    <h4>{{Auth::user()->name}}</h4>--}}
-
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </li>--}}
-{{--                        <li>--}}
-{{--                            <div class="dropdown-divider"></div>--}}
-{{--                            <form action="{{ route('logout') }}" method="POST">--}}
-{{--                                @csrf--}}
-{{--                                <button type="submit" class="btn btn-primary">Chiqish</button>--}}
-{{--                            </form>--}}
-{{--                        </li>--}}
-{{--                    </div>--}}
-{{--                </ul>--}}
-{{--            </li>--}}
-{{--        </ul>--}}
-{{--    </div>--}}
-{{--</nav>--}}
